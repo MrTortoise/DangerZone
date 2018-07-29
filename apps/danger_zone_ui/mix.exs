553c2c60676472cce15d964d862e9591452324ba
John Nicholas <themrtortoise@gmail.com>
2018-07-29 12:32:08 +0100

    added engine to ui as dependency (loads on startup)

--- apps/danger_zone_ui/mix.exs
+++ apps/danger_zone_ui/mix.exs
@@ -41,7 +41,8 @@ defmodule DangerZoneUi.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:danger_zone_engine, in_umbrella: true}
     ]
   end
 end
